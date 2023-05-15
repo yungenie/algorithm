6619b85a3443c09753048e350af1fd3a01a34533
YunJin Gwak <yunjin8931@naver.com>
2023-05-15 18:54:59 +0900

    :seedling: Add 2023-05-15 [Algorithm] 회장 선거 - 해싱,시간파싱 (HashMap 빈도수 해싱)

--- src/study/inflearn/Ex02_13.java
+++ src/study/inflearn/Ex02_13.java
@@ -11,18 +11,18 @@ public class Ex02_13 {
         String answer = " ";
         int n = votes.length;
 
-        // 회장후보목록 : 후보에 따른 추천한 학생명단
-        HashMap<String, List<String>> map = new HashMap<>(); // 학생1 : [추천해준 사람 목록], 학생2 : [추천해준 사람 목록], 학생3 : [추천해준 사람 목록]
+        // 회장후보추천 목록 - {추천받은학생1 : [추천해준 학생 명단], 추천받은학생2 : [추천해준 학생 명단],,,}
+        HashMap<String, List<String>> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             String[] tween = votes[i].split(" ");
             String giver = tween[0];
             String taker = tween[1];
 
-            // 한 후보를 추천한 학생명단 목록
+            // 추천받은 학생의 추천해준 학생 명단
             List<String> list = new ArrayList<>();
             int size = map.getOrDefault(taker,list).size();
             list.add(giver);
-            if (size >= 1) { // 후보를 추천해준 학생이 2명 이상일 때, 기존의 추천목록 + 추천학생
+            if (size >= 1) { // 기존 추천해준 학생 명단 + 추천해준 학생
                 for (int j = 0; j < size; j++) {
                     list.add(map.getOrDefault(taker,list).get(j));
                 }
@@ -31,12 +31,13 @@ public class Ex02_13 {
         }
         //System.out.println("회장후보=투표학생목록" + map);
 
-        // 감사선물 받은 학생명단 - 가장 인기있는 후보들 중, 해당 후보들을 추천한 학생의 빈도수 해싱
+        // 감사선물 받은 학생명단
         Map<String, Integer> thankList = new TreeMap<>(); // 사전순 빠른 학생 기준으로 정렬
         for (Map.Entry<String, List<String>> entry : map.entrySet()) {
             int size = entry.getValue().size();
-            if (size < k) continue;
+            if (size < k) continue; // 회장후보 미달인 경우
 
+            // 회장후보에서 감사선물 해싱
             for (int i = 0; i < size; i++) {
                 String thank = entry.getValue().get(i);
                 thankList.put(thank, thankList.getOrDefault(thank, 0) + 1);
@@ -44,7 +45,7 @@ public class Ex02_13 {
         }
         //System.out.println("감사선물받은 학생 목록"+thankList);
 
-        // 감사선물 최대치
+        // 감사선물을 가장 많이 받은 수
         int max = 0;
         for (Integer value : thankList.values()) {
             if (max < value) {
@@ -52,11 +53,11 @@ public class Ex02_13 {
             }
         }
 
-        // 감사선물 많이 받은 학생 중 사전순 빠른 학생 뽑아내기
+        // 감사선물 가장 많이 받은 학생
         for (Map.Entry<String, Integer> e : thankList.entrySet()) {
-            if (e.getValue() == max) {
+            if (e.getValue() == max) { //사전순으로 빠른 이름
                 answer = e.getKey();
-                break;
+                break; // 첫번째 학생
             }
         }
 
@@ -71,3 +72,8 @@ public class Ex02_13 {
         System.out.println(T.solution(new String[]{"bob tom", "bob park", "park bob", "luis park", "daniel luis", "luis bob", "park luis", "tom bob", "tom luis", "john park", "park john"}, 3));
     }
 }
+
+/*
+- 문제 전체 맥락 집중해서 읽기, 입력예시설명 보면서 문제 차근차근 이해하기.
+
+ */
