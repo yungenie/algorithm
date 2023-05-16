1f2b2d0b691295e28b774003cc8379455415dcb7
YunJin Gwak <yunjin8931@naver.com>
2023-05-16 16:16:45 +0900

    :seedling: Add 2023-05-16 [Algorithm] 문서 도난 - 해싱,시간파싱 (시간흐름을 분으로 변환, ArrayList, Collection.sort())

--- src/study/inflearn/Ex02_14_Answer.java
+++ src/study/inflearn/Ex02_14_Answer.java
@@ -28,15 +28,16 @@ public class Ex02_14_Answer {
     public String[] solution(String[] reports, String times){
         //String[] answer ={};
 
-        // 보안키를 사용한 사람 시간순으로 정렬
+        // 보안키를 사용한 사람과 시간 명단
         ArrayList<Info> tmp = new ArrayList<>();
         for(String x : reports){
             String a = x.split(" ")[0];
             String b = x.split(" ")[1];
             tmp.add(new Info(a, getTime(b)));
         }
+        // 보안키를 사용한 사람 시간순으로 정렬
         Collections.sort(tmp); //tmp = tom:443, tmp = luis:537, tmp = daniel:570, tmp = park:599, tmp = john:923,
-        //tmp.forEach((x)-> System.out.print("tmp = " + x.name + ":" + x.time + ", "));
+        tmp.forEach((x)-> System.out.print("tmp = " + x.name + ":" + x.time + ", "));
 
         // 특정 범위의 시간에 보안키를 사용한 사람들
         int s = getTime(times.split(" ")[0]);
@@ -59,7 +60,7 @@ public class Ex02_14_Answer {
     public static void main(String[] args){
         Ex02_14_Answer T = new Ex02_14_Answer();
         System.out.println(Arrays.toString(T.solution(new String[]{"john 15:23", "daniel 09:30", "tom 07:23", "park 09:59", "luis 08:57"}, "08:33 09:45")));
-        System.out.println(Arrays.toString(T.solution(new String[]{"ami 12:56", "daniel 15:00", "bob 19:59", "luis 08:57", "bill 17:35", "tom 07:23", "john 15:23", "park 09:59"}, "15:01 19:59")));
-        System.out.println(Arrays.toString(T.solution(new String[]{"cody 14:20", "luis 10:12", "alice 15:40", "tom 15:20", "daniel 14:50"}, "14:20 15:20")));
+        //System.out.println(Arrays.toString(T.solution(new String[]{"ami 12:56", "daniel 15:00", "bob 19:59", "luis 08:57", "bill 17:35", "tom 07:23", "john 15:23", "park 09:59"}, "15:01 19:59")));
+        //System.out.println(Arrays.toString(T.solution(new String[]{"cody 14:20", "luis 10:12", "alice 15:40", "tom 15:20", "daniel 14:50"}, "14:20 15:20")));
     }
 }
