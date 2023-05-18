7166c66ed39a092bc8e3854f2d7fcad679f63f72
YunJin Gwak <yunjin8931@naver.com>
2023-05-18 18:15:55 +0900

    :bulb: Add 2023-05-18 [Algorithm] 최대 길이 연속수열 - 자료구조 활용 (HashSet contains() 활용)

--- src/study/inflearn/Ex03_16_Answer.java
+++ src/study/inflearn/Ex03_16_Answer.java
@@ -4,7 +4,11 @@ import java.util.HashSet;
 
 /**
  * 최대 길이 연속수열 - 자료구조 활용
- * HashSet contains()
+ *
+ * set : 중복된 원소를 제거하는 게 특징 입니다. (중복을 허용하지 않음)
+ * HashSet : 기본적으로 해시로 구현되어 있습니다. 모든 검색, 삽입이 해싱으로 O(1)로 모든 연산들이 상수시간의 시간복잡도를 갖습니다.
+ * HashSet.contains() : contains()로 값 유무 확인하면 해싱을 하기 때문에 시간복잡도가 O(1) 입니다.
+ *
  */
 public class Ex03_16_Answer {
     public int solution(int[] nums){
@@ -16,14 +20,14 @@ public class Ex03_16_Answer {
 
         // 가장 긴 연속 수열 찾기
         for(int x : set){
-            // 원소보다 작은 값이 있으면 SKIP
-            if(set.contains(x - 1)) continue;
+            // 연속수열의 시작지점 확인
+            if(set.contains(x - 1)) continue; // 원소보다 작은 값이 있으면 연속수열의 시작지점이 아니므로, SKIP
 
             // 연속수열 찾기
             int cnt = 0;
-            while(set.contains(x)){
+            while(set.contains(x)){ // 연속수열이 거짓이면 멈춤
                 cnt++;
-                x++;
+                x++; // 원소의 1 증가한 숫자가 있는 지 확인
             }
             answer = Math.max(answer, cnt);
         }
