3af84c6a4b3d30e6a133624e30c39d42783d4a51
YunJin Gwak <yunjin8931@naver.com>
2023-05-22 21:55:48 +0900

    :truck: Add 2023-05-14 [Algorithm] 음수가 있는 부분수열 - 해싱,시간파싱 (누적합 빈도수 해싱)

--- src/study/inflearn/lecture02/Ex02_04_Answer.java
+++ src/study/inflearn/lecture02/Ex02_04_Answer.java
@@ -1,10 +1,10 @@
-package study.inflearn;
+package study.inflearn.lecture02;
 
 import java.util.HashMap;
 /**
  *  음수가 있는 부분수열 - 해싱,시간파싱
  */
-public class Ex02_12_Answer {
+public class Ex02_04_Answer {
     public int solution(int[] nums, int m){
         int answer = 0;
         HashMap<Integer, Integer> nH = new HashMap<>();
@@ -19,7 +19,7 @@ public class Ex02_12_Answer {
     }
 
     public static void main(String[] args){
-        Ex02_12_Answer T = new Ex02_12_Answer();
+        Ex02_04_Answer T = new Ex02_04_Answer();
         System.out.println(T.solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));
         System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
         System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2}, 3));
