66bae97f18abb39841e7cd14c678a3e50c3810fa
YunJin Gwak <yunjin8931@naver.com>
2023-05-22 22:00:06 +0900

    :truck: Add 2023-05-16 [Algorithm] 문서 도난 - 해싱,시간파싱 (시간흐름을 분으로 변환, ArrayList, compareTo)

--- src/study/inflearn/lecture02/Ex02_06_Answer.java
+++ src/study/inflearn/lecture02/Ex02_06_Answer.java
@@ -1,4 +1,4 @@
-package study.inflearn;
+package study.inflearn.lecture02;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,7 +19,7 @@ class Info implements Comparable<Info>{
         return this.time - ob.time;
     }
 }
-public class Ex02_14_Answer {
+public class Ex02_06_Answer {
     public int getTime(String time){
         int H = Integer.parseInt(time.split(":")[0]);
         int M = Integer.parseInt(time.split(":")[1]);
@@ -58,9 +58,9 @@ public class Ex02_14_Answer {
     }
 
     public static void main(String[] args){
-        Ex02_14_Answer T = new Ex02_14_Answer();
+        Ex02_06_Answer T = new Ex02_06_Answer();
         System.out.println(Arrays.toString(T.solution(new String[]{"john 15:23", "daniel 09:30", "tom 07:23", "park 09:59", "luis 08:57"}, "08:33 09:45")));
-        //System.out.println(Arrays.toString(T.solution(new String[]{"ami 12:56", "daniel 15:00", "bob 19:59", "luis 08:57", "bill 17:35", "tom 07:23", "john 15:23", "park 09:59"}, "15:01 19:59")));
-        //System.out.println(Arrays.toString(T.solution(new String[]{"cody 14:20", "luis 10:12", "alice 15:40", "tom 15:20", "daniel 14:50"}, "14:20 15:20")));
+        System.out.println(Arrays.toString(T.solution(new String[]{"ami 12:56", "daniel 15:00", "bob 19:59", "luis 08:57", "bill 17:35", "tom 07:23", "john 15:23", "park 09:59"}, "15:01 19:59")));
+        System.out.println(Arrays.toString(T.solution(new String[]{"cody 14:20", "luis 10:12", "alice 15:40", "tom 15:20", "daniel 14:50"}, "14:20 15:20")));
     }
 }
