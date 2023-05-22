7b9872112d5ffbc6faf69cea04d86beed05e275d
YunJin Gwak <yunjin8931@naver.com>
2023-05-22 22:33:44 +0900

    :seedling: Add 2023-05-22 [Algorithm] 선택정렬

---
+++ src/study/inflearn/study/SelectionSort.java
@@ -0,0 +1,36 @@
+package study.inflearn.study;
+
+import java.util.Arrays;
+
+/**
+ * 선택정렬 - 오름차순
+ * - 해당 자리를 선택하고 그 자리에 오는 값을 찾는 알고리즘 입니다.
+ * - 해당 자리에 올 다른 값을 loop돌며 찾아 교환을 합니다.
+ */
+public class SelectionSort {
+
+    public int[] selectionSort(int[] arr){
+        System.out.println("원본 = " + Arrays.toString(arr));
+        int n = arr.length;
+
+        int indexMin, temp;
+        for (int i = 0; i < n - 1; i++) { //위치(index)를 선택합니다.
+            indexMin = i;
+            for (int j = i + 1; j < n; j++) { //i+1번째 원소부터 선택한 위치(index)의 값과 비교를 합니다.
+                if (arr[j] < arr[indexMin]) { //오름차순이므로 현재 선택한 자리에 있는 값보다 순회하고 있는 값이 작다면, 위치(index)를 갱신합니다.
+                    indexMin = j;
+                }
+            }
+            // swap(arr[indexMin], arr[i]) 선택한 위치(index)에 들어가야하는 값의 위치를 갖고 있으므로 서로 교환(swap)해줍니다.
+            temp = arr[indexMin];
+            arr[indexMin] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        SelectionSort T = new SelectionSort();
+        System.out.println("결과 = " + Arrays.toString(T.selectionSort(new int[]{13, 5, 11, 7, 23, 15})));
+    }
+
+}
