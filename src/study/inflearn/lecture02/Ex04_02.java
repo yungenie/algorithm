5f211945598143f26d40d419b21f4f62b7c9daa1
YunJin Gwak <yunjin8931@naver.com>
2023-05-26 16:10:59 +0900

    :seedling: Add 2023-05-26 [Algorithm] 이진수 정렬 - Sorting & Thinking

--- src/study/inflearn/lecture02/Ex04_02.java
+++ src/study/inflearn/lecture02/Ex04_02.java
@@ -10,7 +10,7 @@ public class Ex04_02 {
     public int[] solution(int[] nums){
         int[] answer = new int[nums.length / 2];
 
-        // 정렬
+        // 현수가 만든 수열 정렬
         Arrays.sort(nums);
         //System.out.println("nums = " + Arrays.toString(nums)); // note 1차원 배열 출력은 Arrays.toString
 
@@ -19,10 +19,10 @@ public class Ex04_02 {
         for (int i = 0; i < n-1; i++) {
             int find = nums[i];
             for (int j = i+1; j < n; j++) {
-                if ((find*2) == nums[j]) { // 선택정렬 알고리즘을 활용해서 원소를 두배한 값을 찾기
+                if ((find*2) == nums[j]) { // 선택정렬 알고리즘을 활용해서 원소를 2배한 값을 찾기
                     answer[idx] = find; // 원래 수열 담기
                     idx++;
-                    break; // 첫번째 조건이 같으면 다음 같은 값 찾을 필요 없음
+                    break; // 첫번째 조건이 같으면 다음 같은 값 찾을 필요 없음 (하나의 원소를 2배한 값은 1개뿐, 수열에 중복된 원소가 있다면 중복된 갯수 만큼 2배한 원소가 존재)
                 }
             }
         }
