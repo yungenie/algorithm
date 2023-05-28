5b4e766c7e7e8b4862068cc23c7cfd9e6a0c9cbb
YunJin Gwak <yunjin8931@naver.com>
2023-05-28 15:33:51 +0900

    :recycle: Update 2023-05-27 [Algorithm] 이진수 정렬 - Sorting & Thinking (로직 수정)

--- src/study/inflearn/lecture02/Ex04_02.java
+++ src/study/inflearn/lecture02/Ex04_02.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 /**
  * 수열 찾기 - Sorting & Thinking
  * 소요시간 - 22분
+ * 예외 케이스 추가 - 틀려서 로직 수정
  */
 public class Ex04_02 {
     public int[] solution(int[] nums){
@@ -18,9 +19,13 @@ public class Ex04_02 {
         int n = nums.length, idx = 0;
         for (int i = 0; i < n-1; i++) {
             int find = nums[i];
+            if (find == 0) continue; // 지운 원소 넘어가기
             for (int j = i+1; j < n; j++) {
-                if ((find*2) == nums[j]) { // 선택정렬 알고리즘을 활용해서 원소를 2배한 값을 찾기
+                if (nums[j] == 0) continue; // 지운 원소 넘어가기
+                if ((find*2) == nums[j]) { // 원소를 2배한 값을 찾기
                     answer[idx] = find; // 원래 수열 담기
+                    nums[i] = 0; // 원래 원소 지우기
+                    nums[j] = 0; // 2배한 원소 지우기
                     idx++;
                     break; // 첫번째 조건이 같으면 다음 같은 값 찾을 필요 없음 (하나의 원소를 2배한 값은 1개뿐, 수열에 중복된 원소가 있다면 중복된 갯수 만큼 2배한 원소가 존재)
                 }
@@ -35,5 +40,6 @@ public class Ex04_02 {
         System.out.println(Arrays.toString(T.solution(new int[]{1, 10, 2, 3, 5, 6})));
         System.out.println(Arrays.toString(T.solution(new int[]{1, 1, 6, 2, 2, 7, 3, 14})));
         System.out.println(Arrays.toString(T.solution(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14})));
+        System.out.println(Arrays.toString(T.solution(new int[]{14, 4, 2, 6, 3, 10, 10, 5, 5, 7, 7, 14, 1, 2})));
     }
 }
