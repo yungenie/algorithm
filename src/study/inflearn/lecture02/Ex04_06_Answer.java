85901eb537fe126d925d431b6be07a20f431bfb1
YunJin Gwak <yunjin8931@naver.com>
2023-05-31 20:01:01 +0900

    :seedling: Add 2023-05-31 [algorithm] 이분검색 (정렬먼저)

---
+++ src/study/inflearn/lecture02/Ex04_06_Answer.java
@@ -0,0 +1,17 @@
+package study.inflearn.lecture02;
+
+public class Ex04_06_Answer {
+    public int solution(int[] tasks, long k) {
+        int answer = 0;
+        //System.arraycopy();
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Ex04_06_Answer T = new Ex04_06_Answer();
+        System.out.println(T.solution(new int[]{1, 2, 3}, 5));
+        System.out.println(T.solution(new int[]{8, 5, 2, 9, 10, 7}, 30));
+        System.out.println(T.solution(new int[]{8, 9, 12, 23, 45, 16, 25, 50}, 100));
+    }
+
+}
