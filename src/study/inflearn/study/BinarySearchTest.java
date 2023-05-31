bb97fe2ba92f4c244deda917818db354abf7f6a1
YunJin Gwak <yunjin8931@naver.com>
2023-05-31 20:01:09 +0900

    :seedling: Add 2023-05-31 [algorithm] 이분검색 (정렬먼저)

---
+++ src/study/inflearn/study/BinarySearchTest.java
@@ -0,0 +1,32 @@
+package study.inflearn.study;
+
+import java.util.Arrays;
+
+/**
+ * 이분검색 - 1. 정렬  2. 중간지점 구하기 3. 찾고자 하는 값이 중간보다 아래인지 위인지 찾기
+ */
+public class BinarySearchTest {
+    public static void main(String[] args) {
+        BinarySearchTest T = new BinarySearchTest();
+        System.out.println(T.solution(8, 32, new int[]{23, 87, 65, 12, 57, 32, 99, 81}));
+    }
+
+    public int solution(int n, int m, int[] arr){
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0, rt = n-1;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+            if (arr[mid] > m) rt = mid - 1;
+            else lt = mid + 1;
+        }
+
+        return answer;
+
+    }
+
+}
