49cf24dd10762edd49bfdc0c060b2ba58bc29a00
YunJin Gwak <yunjin8931@naver.com>
2023-06-06 21:50:00 +0900

    :seedling: Add 2023-06-06 [algorithm] 스프링 쿨러 - greedy

--- src/study/inflearn/lecture02/section05/Ex05_03_Answer.java
+++ src/study/inflearn/lecture02/section05/Ex05_03_Answer.java
@@ -9,10 +9,11 @@ public class Ex05_03_Answer {
     public int solution(int n, int[] nums){
         int answer = 0;
         System.out.println("nums = " + nums.length);
-        int[][] line = new int[nums.length + 1][2];
+        //int[][] line = new int[nums.length + 1][2]; // todo N+1의미? new int[n+1] 아닌가?
+        int[][] line = new int[n+1][2];
         for(int i = 0; i <= n; i++){
             line[i][0] = Math.max(0, i - nums[i]); // 음수인 경우 
-            line[i][1] = Math.min(n, i + nums[i]); // 
+            line[i][1] = Math.min(n, i + nums[i]); // n개를 넘는 경우
         }
         Arrays.sort(line, (a, b) -> a[0] - b[0]);
         System.out.println("Arrays.deepToString(l) = " + Arrays.deepToString(line));
