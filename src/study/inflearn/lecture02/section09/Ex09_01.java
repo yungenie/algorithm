7a056c6a3caf6b7c1f324c93606011586de27f7d
YunJin Gwak <yunjin8931@naver.com>
2023-07-04 19:34:27 +0900

    :seedling: Add 2023-07-04 [Algorithm] 사탕 가게 - 다이나믹 프로그래밍

---
+++ src/study/inflearn/lecture02/section09/Ex09_01.java
@@ -0,0 +1,34 @@
+package study.inflearn.lecture02.section09;
+
+
+/**
+ * 사탕 가게 - 다이나믹 프로그래밍
+ * 강사님&영근님 정답코드 보면서 리팩토리
+ */
+public class Ex09_01 {
+    public int solution(int candyCnt, double money, String[] calInfo) {
+
+        // 상근이가 i원을 가지고 얻을 수 있는 최대 칼로리
+        int m = (int) Math.round(money * 100);
+        int[] dp = new int[m+1];
+
+        // DP탐색
+        for (String s : calInfo) {
+            int cal = Integer.parseInt(s.split(" ")[0]); // 사탕 칼로리
+            int price = (int) Math.round(Double.parseDouble(s.split(" ")[1])* 100); // 사탕 가격
+
+            for (int i = price; i < dp.length; i++) {
+                dp[i] = Math.max(dp[i], dp[i-price] + cal);
+            }
+        }
+        return dp[m];
+    }
+
+    public static void main(String[] args) {
+        Ex09_01 T = new Ex09_01();
+        System.out.println(T.solution(2, 8.00, new String[]{"700 7.00", "199 2.00"})); // 796
+        System.out.println(T.solution(3, 8.00, new String[]{"700 7.00", "299 3.00", "499 5.00"})); // 798
+    }
+
+
+}
---
+++ src/study/inflearn/lecture02/section09/Ex09_01_Answer.java
@@ -0,0 +1,44 @@
+package study.inflearn.lecture02.section09;
+
+import java.util.Scanner;
+
+/**
+ * 사탕 가게 - 다이나믹 프로그래밍
+ */
+public class Ex09_01_Answer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = sc.nextInt(); // 사탕 종류
+            int m = (int) Math.round(sc.nextDouble() * 100); // 돈의 양
+            if (n == 0 && m == 0) {
+                break;
+            }
+            // DP탐색
+            int[] dy = new int[m + 1];
+            for (int i = 0; i < n; i++) {
+                int c = sc.nextInt(); // 사탕 칼로리
+                int p = (int) Math.round(sc.nextDouble() * 100); // 사탕 가격
+                for (int j = p; j <= m; j++) {
+                    dy[j] = Math.max(dy[j], dy[j - p] + c);
+                }
+            }
+            System.out.println(dy[m]);
+        }
+    }
+}
+/*
+예제 입력1
+2 8.00
+700 7.00
+199 2.00
+3 8.00
+700 7.00
+299 3.00
+499 5.00
+0 0.00
+
+예제 출력1
+796
+798
+ */
