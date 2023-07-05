ac8761a47c8f30b554e393b7ea8bc87b5c097647
YunJin Gwak <yunjin8931@naver.com>
2023-07-05 16:54:07 +0900

    :bug: Update 2023-07-05 [Algorithm] Integer.MAX_VALUE + 1 정수형 범위 넘어가는 버그 수정

--- src/study/inflearn/lecture01/section10/Ex10_05.java
+++ src/study/inflearn/lecture01/section10/Ex10_05.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 /**
  * 동전교환(냅색 알고리즘) - dynamic programming(동적계획법)
  * 소요시간 : 23분
+ * -> dp[0] = 0 안해서 틀림
+ * -> 추가 입력케이스 틀림
+ * -> dp배열의 Interger.MAX_VALUE 초기화 틀림
+ * -> 이 문제는 Ex10_05_Answer보기
  */
 public class Ex10_05 {
     public static void main(String[] args) {
@@ -24,15 +28,28 @@ public class Ex10_05 {
         int m = sc.nextInt();
         int[] dp = new int[m+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
-        //dp[0] = 0; // note MAX_VALUE로 초기화 되었기 때문에 dp[0]은 0으로 초기화 해줘야한다.
+        dp[0] = 0; // note MAX_VALUE로 초기화 되었기 때문에 dp[0]은 0으로 초기화 해줘야한다.
 
         for (int money : mArr) {
             // DP탐색
             for (int i = money; i < dp.length; i++) {
-                dp[i] = Math.min(dp[i], i/money + i%money); // 거슬러 줄 동전의 최소개수
-                //dp[i] = Math.min(dp[i], dp[i-money] + 1); // 거슬러 줄 동전의 최소개수
+                dp[i] = Math.min(dp[i], i/money + i%money); // 틀림
             }
         }
         System.out.println(">> " + dp[m]);
     }
 }
+
+/*
+입력예제1
+3
+1 2 5
+15
+>> 3
+
+입력예제2
+3
+2 3 4
+15
+>> 4
+ */
--- src/study/inflearn/lecture01/section10/Ex10_05_Answer.java
+++ src/study/inflearn/lecture01/section10/Ex10_05_Answer.java
@@ -23,14 +23,18 @@ public class Ex10_05_Answer {
         // 금액
         int m = sc.nextInt();
         int[] dp = new int[m+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        //dp[0] = 0; //왜?
+        /*
+            Interger 범위는 -2,147,483,648 ~ 2,147,483,647 이므로, 2,147,483,647+1 int 정수형의 범위를 넘어가므로 -2,147,483,648이 된다.
+            그러므로 dp배열의 초기값을 Integer.MAX_VALUE-1 하거나 M으로 해야한다.
+         */
+        Arrays.fill(dp, Integer.MAX_VALUE-1);
+        //Arrays.fill(dp, m);
+        dp[0] = 0;
 
         for (int money : mArr) {
             // DP탐색
             for (int i = money; i < dp.length; i++) {
-                dp[i] = Math.min(dp[i], i/money + i%money); // 거슬러 줄 동전의 최소개수
-                //dp[i] = Math.min(dp[i], dp[i-money] + 1); // 거슬러 줄 동전의 최소개수
+                dp[i] = Math.min(dp[i], dp[i-money] + 1); // 거슬러 줄 동전의 최소개수
             }
         }
         System.out.println(">> " + dp[m]);
