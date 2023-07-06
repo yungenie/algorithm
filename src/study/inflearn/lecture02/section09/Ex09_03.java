158b21d6fa6ae6f8ffc888c85d548f3f998cf04f
YunJin Gwak <yunjin8931@naver.com>
2023-07-06 17:03:55 +0900

    :seedling: Add 2023-07-06 [Algorithm] 계단 오르기 다이나믹 프로그래밍 (냅색)

---
+++ src/study/inflearn/lecture02/section09/Ex09_03.java
@@ -0,0 +1,43 @@
+package study.inflearn.lecture02.section09;
+
+import java.util.Scanner;
+
+/**
+ * 계단 오르기 다이나믹 프로그래밍 (백준 - 2579)
+ * 강사님 해법 듣고 재도전 (소요시간 : 19분)
+ */
+public class Ex09_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 계단의 개수
+        int[] score = new int[N+1]; // 계단 점수 배열
+        for (int i = 1; i <= N; i++) {
+            score[i] = sc.nextInt(); // 계단 점수 초기화
+        }
+
+        // DP탐색
+        int[] dp = new int[N+1]; // 다이나믹 테이블
+        for (int i = 1; i < dp.length; i++) {
+            if (i == 1) { // 첫번째 계단
+                dp[1] = score[1];
+                continue;
+            }
+            if (i == 2) { // 두번째 계단 (두계단씩 오르는 것 보다, 2개의 계단을 밟는게 점수가 더 큼)
+                dp[2] = score[1] + score[2];
+                continue;
+            }
+            dp[i] = Math.max(dp[i - 2] + score[i], dp[i - 3] + score[i - 1] + score[i]);
+        }
+        System.out.println("dp = " + dp[N]);
+    }
+}
+/*
+6
+10
+20
+15
+25
+10
+20
+>> 75
+ */
---
+++ src/study/inflearn/lecture02/section09/Ex09_03_Answer.java
@@ -0,0 +1,38 @@
+package study.inflearn.lecture02.section09;
+
+import java.util.Scanner;
+
+/**
+ * 계단 오르기 다이나믹 프로그래밍 (백준 - 2579)
+ */
+public class Ex09_03_Answer {
+    public int solution(int n, int[] score) {
+        int[] dy = new int[n + 1];
+        dy[1] = score[1]; // 첫번째 계단 점수
+        if (n > 1) dy[2] = score[1] + score[2]; // 두번째 계단 점수 (두 계단 점프 하는 것 보다 연속으로 오르는 점수가 항상 큼)
+        for (int i = 3; i <=n ; i++) {
+            dy[i] = Math.max(dy[i - 2] + score[i], dy[i - 3] + score[i - 1] + score[i]);
+        }
+        return dy[n];
+    }
+    public static void main(String[] args) {
+        Ex09_03_Answer T = new Ex09_03_Answer();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 총 계단의 개수
+        int[] score = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            score[i] = sc.nextInt(); // 각 계단에 씌여진 점수
+        }
+        System.out.println(T.solution(n, score));
+    }
+}
+/*
+6
+10
+20
+15
+25
+10
+20
+>> 75
+ */
