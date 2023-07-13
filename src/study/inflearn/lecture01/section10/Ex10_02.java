5ee4c0e5f8e8303a475160c87aa6d7f1b2d36c11
YunJin Gwak <yunjin8931@naver.com>
2023-07-13 11:09:48 +0900

    :seedling: Add 2023-07-12 [Algorithm] 돌다리 건너기 - 다이나믹 프로그래밍

---
+++ src/study/inflearn/lecture01/section10/Ex10_02.java
@@ -0,0 +1,29 @@
+package study.inflearn.lecture01.section10;
+
+import java.util.Scanner;
+
+/**
+ * 돌다리 건너기 (백준)
+ * 틀려서 강사님 강의 듣고 수정함.
+ * - 계단 오르기를 풀었다면 간파를 하고 바로 풀어버렸어야하는 문제. 간파를 못했다..
+ * - 계단 오르기와 다른 점은 계단 오르기는 마지막 계단까지 올라갈 수 있는 방법으로 dp[N]까지 계산하면 된다.
+ * - 돌다리 건너기는 모든 돌다기를 건너야하기 때문에 마지막 돌다리를 건너 땅까지 도착했을때의 경우의 수 dp[N+1]를 구해야한다.
+ * - 마지막 돌다리 건너는 수까지 생각하지 못했다. 그래서 다른 점화식을 계속 생각해봤음.
+ */
+public class Ex10_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N + 2]; //✨
+        //dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <=N + 1 ; i++) { //✨
+            dp[i] = dp[i - 1] + dp[i - 2]; //✨
+        }
+
+        System.out.println(dp[N+1]); //✨
+    }
+}
+
