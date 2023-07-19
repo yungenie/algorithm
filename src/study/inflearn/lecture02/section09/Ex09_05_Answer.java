026c4a3c23a98147cf0444ece37c83aeab98b54b
YunJin Gwak <yunjin8931@naver.com>
2023-07-19 13:21:52 +0900

    :memo: Update 2023-07-19 [Algorithm] 풀이 예시 주석 추가

--- src/study/inflearn/lecture02/section09/Ex09_05_Answer.java
+++ src/study/inflearn/lecture02/section09/Ex09_05_Answer.java
@@ -6,17 +6,29 @@ package study.inflearn.lecture02.section09;
 public class Ex09_05_Answer {
     public int solution(int n, int[] s) {
 
-        int[] nums = new int[n + 1];
+        // 입력 데이터 배열
+        int[] nums = new int[n + 1]; //✨
         for (int i = 1; i <=n ; i++) {
             nums[i] = s[i-1];
         }
-        int[][] dp = new int[n + 1][n + 1];
-        for (int i = 1; i < n; i++) {
+
+        // i번째부터 j번째까지 부분수열에서 팰린드롬을 만들기위해 끼워넣어야할 수의 최소 개수
+        int[][] dp = new int[n + 1][n + 1]; //✨
+        for (int i = 1; i < n; i++) { //✨
             for (int j = 1; j <= n-i; j++) { //✨
+                /*
+                    예시)
+                    1 2 3 4 1 j번째(1)와 j+i(1)번째 원소가 같은 경우 (양끝이 같은 경우)
+                        - 2 3 4 j+1번째부터 j+i-1번째까지 dp값(이미 최소값이 구해져있음)
+                    4 5 2 7 3 모두 다른 원소의 경우
+                        - 4 | 5 2 7 3, 4를 끼워넣는 개수(1) + j+1번째부터 j+i번째까지 dp값
+                        - 4 5 2 7 | 3, j번째부터 j+i-1번째가지 dp값 + 3을 끼워넣는 개수(1)
+                        - 2가지 경우의 수 중 가장 작은 수가 j번째부터 j+i번째까지의 최소 개수
+                 */
                 if (nums[j] == nums[j+i]) { //✨
-                    dp[j][j+i] = dp[j+1][j+i-1];
+                    dp[j][j+i] = dp[j+1][j+i-1]; //✨
                 } else {
-                    dp[j][j+i] = Math.min(dp[j+1][j+i], dp[j][j+i-1]) + 1;
+                    dp[j][j+i] = Math.min(dp[j+1][j+i], dp[j][j+i-1]) + 1; //✨
                 }
             }
         }
--- src/study/inflearn/lecture02/section09/Ex09_06_Answer_02.java
+++ src/study/inflearn/lecture02/section09/Ex09_06_Answer_02.java
@@ -11,20 +11,20 @@ public class Ex09_06_Answer_02 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // 벼의 가치 0 ~ n+1
-        int[] nums = new int[n + 1];
+        // 벼의 가치 n+1 잡는 이유는 입력 데이터의 순서를 1부터하기 위함
+        int[] nums = new int[n + 1]; //✨
         for (int i = 1; i <= n; i++) {
             nums[i] = sc.nextInt();
         }
         // 벼의 가치 누적합
-        int[] sums = new int[n + 1];
+        int[] sums = new int[n + 1]; //✨
         sums[1] = nums[1]; //✨
         for (int i = 2; i <= n; i++) { //✨
             sums[i] = sums[i - 1] + nums[i]; //✨
         }
 
-        // dp정의
-        int[][] dp = new int[n + 1][n + 1];
+        // i번째 벼부터 j번째 벼까지 수확했을 때 얻는 최대 이익
+        int[][] dp = new int[n + 1][n + 1]; //✨
         // 벼 1개 경우
         for (int i = 1; i <= n; i++) {
             dp[i][i] = nums[i];
@@ -32,7 +32,20 @@ public class Ex09_06_Answer_02 {
         // 벼 2개이상 경우
         for (int i = 1; i < n ; i++) { //✨
             for (int j = 1; j <= n-i; j++) { //✨
-                dp[j][j + i] = Math.max(dp[j][j + i - 1], dp[j + 1][j + i]) + (sums[j + i] - sums[j - 1]); //✨
+                /*
+                    예시)
+                    3 1 5 2 일 때
+                        - 3 | 1 5 2, j+1번째부터 j+i번째까지의 dp값 + (3x1, 3을 먼저 수확) + (1+5+2, 나머지 벼들도 번째 증가되는 효과)
+                        - 3 1 5 | 2, j번째부터 j+i+1번째까지의 dp값 + (2x1, 2를 먼저 수확) + (3+1+5, 나머지 벼들도 번째 증가되는 효과)
+                        - 결국, 부분 경우의 수 전체(3+1+5+2)를 더하면 모두 한번씩 번째가 증가된다.
+                            - 3 | 1 5 2, 1 5 2가 1x3 + 5x2 + 2x1의 순서로 벼를 수확했을 때
+                                - 1 5 2 dp값에서 3의 벼를 첫번째로 수확하는 경우를 합산해야한다.
+                                - 3x1 + 1x4 + 5x3 + 2x2로 계산되어야 하기 때문에 3+1+5+2 주어진 수를 모두 합하면 된다.
+                        - 벼의 가치 누적합의 배열을 사용한다.
+                            - j번째~j+i번째의 누적합은 누적배열의 j+i번째 - j-1번째를 빼면 된다.
+                 */
+                dp[j][j + i] = Math.max(dp[j][j + i - 1], dp[j + 1][j + i])
+                        + (sums[j + i] - sums[j - 1]); //✨
             }
         }
 
