85d24a88fc349aab792b5b21523c17bb53c4a1fc
YunJin Gwak <yunjin8931@naver.com>
2023-08-16 21:30:17 +0900

    :seedling: Add 2023-08-16 [SQL] Join 문제

--- src/study/sql/programmers/join.sql
+++ src/study/sql/programmers/join.sql
@@ -117,3 +117,26 @@ SELECT A.NAME, A.DATETIME
          ON A.ANIMAL_ID = B.ANIMAL_ID
  WHERE B.ANIMAL_ID IS NULL
  ORDER BY A.DATETIME ASC LIMIT 3;
+
+
+-- Level 4 보호소에서 중성화한 동물 (소요시간 30분)
+-- 보호소에서 중성화 수술을 거친 동물 정보를 알아보려 합니다.
+-- 보호소에 들어올 당시에는 중성화1되지 않았지만,
+-- 보호소를 나갈 당시에는 중성화된 동물의
+-- 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성해주세요.
+
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
+FROM ANIMAL_INS A
+         JOIN ANIMAL_OUTS B
+              ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.SEX_UPON_INTAKE LIKE '%Intact%'
+  AND (B.SEX_UPON_OUTCOME LIKE '%Spayed%' OR B.SEX_UPON_OUTCOME LIKE '%Neutered%') -- () 괄호 빼먹어서 계속 틀렸음.
+ORDER BY ANIMAL_ID ASC;
+
+
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
+FROM ANIMAL_INS A
+         JOIN ANIMAL_OUTS B
+              ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.SEX_UPON_INTAKE LIKE '%Intact%'
+  AND B.SEX_UPON_OUTCOME NOT LIKE '%Intact%';
