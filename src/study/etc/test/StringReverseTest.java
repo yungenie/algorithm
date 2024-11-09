af5fed17bf64fb0046c179ca4b63f026487c193f
gwak-yunjin <yunjin8931@gmail.com>
2024-11-09 21:27:26 +0900

    :sparkles: 문자열 반전

--- src/study/etc/test/StringReverseTest.java
+++ src/study/etc/test/StringReverseTest.java
@@ -6,17 +6,26 @@ import java.io.InputStreamReader;
 public class StringReverseTest {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        String text = br.readLine();
 
         // String 문자열 반전 시키기
+        StringReverseTest st = new StringReverseTest();
         System.out.println("방법1");
+        st.StringToCharReverse(text);
+
+        System.out.println("\n방법2");
+        System.out.println(st.StringBuilderReserve(text));
+
+    }
+
+    private void StringToCharReverse(String str){
         for (int i = str.length()-1; i >= 0; i--) {
             System.out.print(str.charAt(i));
         }
-
-        System.out.println("\n방법2");
-        StringBuilder sb = new StringBuilder(str);
-        System.out.println(sb.reverse());
-
     }
+    private String StringBuilderReserve(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+
 }
