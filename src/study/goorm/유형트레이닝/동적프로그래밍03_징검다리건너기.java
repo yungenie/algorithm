522c3a3c37830d1c0fb15f36d2247d93800e4ce6
gwak-yunjin <yunjin8931@gmail.com>
2024-11-23 22:02:06 +0900

    :sparkles: 구름 동적 프로그래밍 - 징검다리건너기 (레벨3)

--- src/study/goorm/유형트레이닝/동적프로그래밍03_징검다리건너기.java
+++ src/study/goorm/유형트레이닝/동적프로그래밍03_징검다리건너기.java
@@ -9,11 +9,11 @@ public class 동적프로그래밍03_징검다리건너기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] water = new int[N+2];
+        int[] water = new int[N+2]; // 징검다리 출발전, 도착후까지 포함.
         int[] dp = new int[N+2];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) { // 징검다리 부분만 데이터 저장. 출발, 도착은 0으로 세팅
             water[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -21,11 +21,13 @@ public class 동적프로그래밍03_징검다리건너기 {
         dp[2] = water[2];
         dp[3] = water[3];
 
+        // 최대 점프력 3으로, 징검다리 전에서 4까지가 최대 점프이므로 4부터 시작.
         int min = 0;
         for (int i = 4; i < N+2; i++) {
-            min = dp[i-1] + water[i];
+            min = water[i] + dp[i-1];
             for (int j = i-3; j < i-1; j++) {
-                min = Math.min(dp[j] + water[i], min);
+                // 현재 + 전, 전전, 전전전 3가지 경우 중 제일 작은 값 세팅
+                min = Math.min(water[i] + dp[j], min);
             }
             dp[i] = min;
         }
