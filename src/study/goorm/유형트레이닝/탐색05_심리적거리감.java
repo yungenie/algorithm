874c60c17b7202ad6d22fcc361aad0f47bc97f7c
gwak-yunjin <yunjin8931@gmail.com>
2024-12-11 17:41:18 +0900

    :sparkles: 구름 탐색 - 심리적 거리감 (레벨3)

--- src/study/goorm/유형트레이닝/탐색05_심리적거리감.java
+++ src/study/goorm/유형트레이닝/탐색05_심리적거리감.java
@@ -7,75 +7,68 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 4시 05분 시작 - 4시 46분 완료. 성공!
+ */
 public class 탐색05_심리적거리감 {
     public static void main(String[] args) throws Exception {
+        // 입력 값 읽기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        /**
-         * 4시 05분 시작 - 4시 46분 완료. 성공!
-         *
-         * 최수 다리 개수 + 두 섬 번호 차이(절대값)
-         *
-         * k번 가장 거리감이 먼 섬. (섬 번호 높은)
-         *
-         * 트리 문제.
-         */
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()); // 섬의 개수
         int M = Integer.parseInt(st.nextToken()); // 다리의 개수
         int K = Integer.parseInt(st.nextToken()); // 구름이 섬의 번호
 
-        // 노드의 단방향 관계
+        // 그래프의 연결 관계 - 인접 행렬 사용 (2차원 배열)
         boolean[][] relation = new boolean[N+1][N+1];
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int n1 = Integer.parseInt(st.nextToken());
-            int n2 = Integer.parseInt(st.nextToken());
-            relation[n1][n2] = true;
+            int n1 = Integer.parseInt(st.nextToken()); // 행(시작노드)
+            int n2 = Integer.parseInt(st.nextToken()); // 열(도착노드)
+            relation[n1][n2] = true; // 단방향 다리 연결
         }
 
         // 시작지점
         int startNode = K;
 
-        // 거리 비용
+        // 최단 거리 비용
         int[] dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[0] = 0; // 0지점은 제외
-        dist[startNode] = 0;
+        Arrays.fill(dist, Integer.MAX_VALUE); // 최단거리 초기화 : 최단거리 구하기 위해 최대값으로 초기화
+        dist[startNode] = 0; // 시작 노드의 거리는 0
 
-        // BFS
+        // BFS를 통한 최단 거리 계산
         Queue<Integer> q = new LinkedList<>();
-        q.offer(startNode); // 지점
+        q.offer(startNode);
 
         while (!q.isEmpty()) {
+            // 시작 지점부터 도착하는 모든 노드의 최단거리 구하기
             int now = q.poll();
             for (int i = 1; i < N+1; i++) {
-                if (relation[now][i] && dist[i] > dist[now] + 1) {
+                // 연결된 노드이고, 더 짧은 거리일 경우 업데이트
+                if (relation[now][i] &&
+                        dist[i] > dist[now] + 1) { // 도착하는 노드의 최단거리 > 시작노드의 기존 최단거리 + 시작노드에서 도착노드까지 이동 거리(1)
                     dist[i] = dist[now] + 1;
                     q.offer(i);
                 }
             }
         }
 
-        // 심리적 거리감 : 최소 다리 개수 + 두 섬 번호 차이의 절대값
+        // 심리적 거리감 계산 (최소 다리 개수 + 두 섬 번호 차이의 절대값)
         int[] resultDist = new int[N+1];
-        resultDist[0] = 0;
         for (int i = 1; i < N+1; i++) {
-            int numDiff = Math.abs(K - i);
-            resultDist[i] = dist[i] + numDiff;
+            resultDist[i] = dist[i] + Math.abs(K - i);
         }
 
-        // 구름이의 K번 섬에서 가장 먼 섬
+        // 구름이의 K번 섬에서 가장 심리적 거리감이 먼 섬 찾기
         int maxDist = Arrays.stream(resultDist).max().getAsInt();
 
-        // 모든 섬에 심리적 거리감이 없는 경우
+        // 심리적 거리가 먼 섬이 없으면 -1 출력
         if (maxDist == 0) {
             System.out.println(-1);
             return;
         }
 
-        // 심리적 거리감 동일한 섬 존재하는 경우, 높은 번호 섬 출력
+        // 심리적 거리감이 동일한 섬 중 높은 번호 섬 찾기
         int landNumber = -1;
         for (int i = 1; i < N+1; i++) {
             if (resultDist[i] == maxDist) {
@@ -84,8 +77,6 @@ public class 탐색05_심리적거리감 {
         }
 
         System.out.println(landNumber);
-
-
 
     }
 }
