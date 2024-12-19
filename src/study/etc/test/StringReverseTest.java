3d6a082879e118f2441ed0ae9230ecfda54df6d8
gwak-yunjin <yunjin8931@gmail.com>
2024-12-19 11:58:42 +0900

    :sparkles: String Reverse 테스트

--- src/study/etc/test/StringReverseTest.java
+++ src/study/etc/test/StringReverseTest.java
@@ -3,8 +3,17 @@ package study.etc.test;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 문자열을 반전 시켜 출력해주세요.
+ */
 public class StringReverseTest {
     public static void main(String[] args) throws Exception {
+
+        String mon = null;
+        String child = mon + "papa";
+        System.out.println(child);
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String text = br.readLine();
 
