04d6808c38123181c01fcf9392886870340993a8
gwak-yunjin <yunjin8931@gmail.com>
2024-12-19 12:24:37 +0900

    :sparkles: 복수의 문자열 중복 제거

--- src/study/etc/test/StringDuplicationRemoveTest.java
+++ src/study/etc/test/StringDuplicationRemoveTest.java
@@ -2,6 +2,7 @@ package study.etc.test;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * 복수의 문자열이 주어졌을때, 중복된 문자를 제거하는 방법
@@ -11,11 +12,6 @@ import java.util.Scanner;
 public class StringDuplicationRemoveTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String stt = "set";
-        stt.isBlank();
-        stt.isEmpty();
-
 
         Object[] array = Arrays.stream(sc.nextLine().split(""))
                 .filter(s -> !s.isBlank())
