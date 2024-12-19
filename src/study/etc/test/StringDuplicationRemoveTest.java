c848a37db23eb7302d6a5f386a14d64ae11971f0
gwak-yunjin <yunjin8931@gmail.com>
2024-12-19 12:00:09 +0900

    :sparkles: 복수의 문자열 중복 제거

---
+++ src/study/etc/test/StringDuplicationRemoveTest.java
@@ -0,0 +1,29 @@
+package study.etc.test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 복수의 문자열이 주어졌을때, 중복된 문자를 제거하는 방법
+ * 입력 : hello, holy
+ * 출력 : heloy
+ */
+public class StringDuplicationRemoveTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String stt = "set";
+        stt.isBlank();
+        stt.isEmpty();
+
+
+        Object[] array = Arrays.stream(sc.nextLine().split(""))
+                .filter(s -> !s.isBlank())
+                .toArray();
+
+        Arrays.stream(array)
+                .distinct()
+                .forEach(System.out::print);
+
+    }
+}
