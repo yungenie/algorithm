f068ce4d8aaa60f2f5d43ffaa7f3f78e96891251
gwak-yunjin <yunjin8931@gmail.com>
2024-12-28 17:15:12 +0900

    :recycle: 구름 기초 - 밀도 정렬 (레벨2)

--- src/study/goorm/기초트레이닝/기초17_밀도정렬.java
+++ src/study/goorm/기초트레이닝/기초17_밀도정렬.java
@@ -11,13 +11,12 @@ public class 기초17_밀도정렬 {
 
         /**
          * 밀도 = 무게/부피
-         * 밀도가 같다면, 부피가 무거운것.
-         * 밀도도 같고, 부피도 같으면 번호가 더 작은 것.
+         * 밀도가 같다면, 무게가 무거운 번호
+         * 밀도와 부피도 같으면 더 작은 번호
          */
 
         int N = Integer.parseInt(br.readLine());
 
-        int d = Integer.MIN_VALUE;
         int[] wArr = new int[N+1];
         double[] dArr = new double[N+1];
         for (int i = 1; i <= N; i++) {
@@ -29,31 +28,33 @@ public class 기초17_밀도정렬 {
             dArr[i] = w/(double) v;
         }
 
+        // 가장 높은 밀도 구하기.
         double dmax = Arrays.stream(dArr).max().getAsDouble();
 
         int result = -1;
-        int max = Integer.MIN_VALUE;
-        int minIndex = Integer.MAX_VALUE;
+        int wMax = Integer.MIN_VALUE;
+        int number = Integer.MAX_VALUE;
         for (int i = 1; i <= N ; i++) {
 
             // 가장 높은 밀도가 같은 경우
             if (dmax == dArr[i]) {
-                minIndex = Math.min(minIndex, i);
+                // 가장 높은 밀도 번호 초기화
+                number = Math.min(number, i);
 
-                // 가장 무거운 요소의 번호
-                if (max < wArr[i]) {
-                    max = wArr[i];
+                // 가장 무거운 요소의 번호 구하기
+                // 첫 반복문 순서때 무게 초기화, 다음 순서때 더 큰 무게로 초기화
+                if (wMax < wArr[i]) {
+                    wMax = wArr[i];
                     result = i;
-                // 가장 무거운 무게가 동일한 경우 작은 번호
-                } else if (max == wArr[i]){
-                    result = minIndex;
+                // 가장 무거운 무게가 동일한 경우 작은 번호 구하기
+                } else if (wMax == wArr[i]){
+                    result = number;
                     break;
                 }
             }
         }
 
         System.out.println(result);
-
 
     }
 }
