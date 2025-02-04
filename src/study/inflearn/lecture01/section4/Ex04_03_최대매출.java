d3574151c5e8ac7078f4b38686be2a204e4e260d
gwak-yunjin <yunjin8931@gmail.com>
2025-02-04 13:17:53 +0900

    :poop: 최대 매출 (시간초과)

---
+++ src/study/inflearn/lecture01/section4/Ex04_03_최대매출.java
@@ -0,0 +1,38 @@
+package study.inflearn.lecture01.section4;
+
+import java.util.Scanner;
+
+/**
+ * 틀림.
+ * TC 1개 Time Limit Exce..
+ */
+public class Ex04_03_최대매출 {
+
+    public int solution(int n, int m, int[] saleInfo) {
+        int maxSales = 0;
+
+        for (int i = 0; i < n-m; i++) {
+            int sum = 0;
+            for (int j = i; j < i+m; j++) {
+                sum += saleInfo[j];
+            }
+            maxSales = Math.max(maxSales, sum);
+        }
+
+        return maxSales;
+    }
+
+    public static void main(String[] args) {
+        Ex04_03_최대매출 T = new Ex04_03_최대매출();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 매출 기록 일 수
+        int m = sc.nextInt(); // 최대 매출 일 수
+        int[] saleInfo = new int[n];
+        for (int i = 0; i < n; i++) {
+            saleInfo[i] = sc.nextInt();
+        }
+
+        System.out.println(T.solution(n, m, saleInfo));
+
+    }
+}
