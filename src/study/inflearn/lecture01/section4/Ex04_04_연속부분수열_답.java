1328c3ef0b8c37247da5070bdaf93577d0cb6fe6
gwak-yunjin <yunjin8931@gmail.com>
2025-02-04 16:38:53 +0900

    :sparkles: 연속 부분 수열

---
+++ src/study/inflearn/lecture01/section4/Ex04_04_연속부분수열_답.java
@@ -0,0 +1,35 @@
+package study.inflearn.lecture01.section4;
+
+import java.util.Scanner;
+
+public class Ex04_04_연속부분수열_답 {
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0, sum = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if (sum == m) answer++;
+            while (sum > m) {
+                sum -= arr[lt++];
+                if (sum == m) answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex04_04_연속부분수열_답 T = new Ex04_04_연속부분수열_답();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(T.solution(n, m, arr));
+
+    }
+
+
+}
