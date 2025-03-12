5df36e6d324044459b987399d99f9e50d3f8c6ab
gwak-yunjin <yunjin8931@gmail.com>
2025-03-12 21:38:51 +0900

    :sparkles: HashMap computeIfAbsent(), computeIfPresent() 테스트

--- src/study/etc/test/HashMapTest.java
+++ src/study/etc/test/HashMapTest.java
@@ -13,12 +13,20 @@ public class HashMapTest {
         map.put("C", 3);
 
         // 값 가져오기 테스트
+        System.out.println("========값 가져오기========");
         System.out.println(map.get("A"));
+        System.out.println(map.get("FF"));
         System.out.println(map.getOrDefault("F", -1));
 
         // 값 확인 후 반환 테스트
-        System.out.println(map.putIfAbsent("A", 7)); // 값이 존재하는 경우 원래값 반환
-        System.out.println(map.putIfAbsent("D", 4)); // 값이 없으면 null 반환
+        System.out.println("========값 저장하기========");
+        System.out.println(map.putIfAbsent("A", 7)); // key 존재하는 경우 기존 value 반환
+        System.out.println(map.putIfAbsent("D", 4)); // key 존재하지 않는 경우 지정한 value 저장하고 null 반환
+        System.out.println(map.computeIfAbsent("C", v -> 7)); // key 존재하는 경우 mappingFunction 무시하고 기존 value 반환
+        System.out.println(map.computeIfAbsent("E", v -> 7)); // key 존재하지 않는 경우 mappingFunction 진행 후 변경 value 반환
+        System.out.println(map.computeIfPresent("A", (key, value) -> value + 100)); // key 존재하는 경우 remappingFunction 진행 후 변경 value 반환
+        System.out.println(map.compute("B", (key, value) -> value + 9)); // key 존재하는 경우만 remappingFunction 진행, key 없으면 예외 발생
+        System.out.println(map);
 
         // 반복문 key, value 출력
         for (Map.Entry<String, Integer> entry : map.entrySet()){
@@ -26,6 +34,10 @@ public class HashMapTest {
             System.out.println(entry.getValue());
         }
 
+        HashMap<Integer, HashMap<Integer, Integer>> mapMap = new HashMap<>();
+        HashMap<Integer, Integer> innerMap = mapMap.computeIfAbsent(1, value -> new HashMap<>());
+        innerMap.put(2, innerMap.getOrDefault(2, 0) + 1);
+        System.out.println(mapMap);
 
     }
 }
