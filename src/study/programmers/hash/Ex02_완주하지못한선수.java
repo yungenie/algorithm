525f6ad983189da0735853a9ea1129a45407dfd5
gwak-yunjin <yunjin8931@gmail.com>
2025-03-14 13:20:54 +0900

    :sparkles: Hashset 활용 문제

---
+++ src/study/programmers/hash/Ex02_완주하지못한선수.java
@@ -0,0 +1,37 @@
+package study.programmers.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 정답. 13분 소요
+ */
+public class Ex02_완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        // 참가자 HashMap
+        // 완료자 for문 돌면서 key 삭제
+        Map<String, Integer> map = new HashMap<>();
+        for(String pName : participant) {
+            // 없으면 1, 있으면 + 1
+            map.put(pName, map.getOrDefault(pName, 0) + 1);
+        }
+
+        for(String cName : completion) {
+            // 있으면 -1, 없으면 냅둬.
+            map.computeIfPresent(cName, (k, v) -> v - 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 0) {
+                answer = entry.getKey();
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
